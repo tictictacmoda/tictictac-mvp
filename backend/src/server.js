ac93fceeecd9ac6be98a7c06060356c9175764ff
Bruno Oliveira <tictictacmoda@gmail.com>
2026-06-05 21:40:37 -0400

    cors definitivo v2

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,10 +6,14 @@ const authRoutes = require('./routes/auth.routes')
 const app = express()
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS')
-  res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization')
-  if (req.method === 'OPTIONS') return res.sendStatus(200)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+  res.setHeader('Access-Control-Max-Age', '86400')
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
   next()
 })
 
