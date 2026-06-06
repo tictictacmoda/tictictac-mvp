b290a662c9f7ec1de664cc44d96aef17550894f0
Bruno Oliveira <tictictacmoda@gmail.com>
2026-06-05 21:50:49 -0400

    bind 0.0.0.0

--- backend/src/server.js
+++ backend/src/server.js
@@ -23,6 +23,6 @@ app.use('/api/auth', authRoutes)
 app.use('/api/participantes', participantesRoutes)
 
 const PORT = process.env.PORT || 3001
-app.listen(PORT, () => {
-  console.log(`Servidor rodando em http://localhost:${PORT}`)
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Servidor rodando em http://0.0.0.0:${PORT}`)
 })
